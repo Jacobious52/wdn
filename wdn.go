3fa6625785ed1a2ff1c6d309005d9575126b070e
Jacob Gonzalez <jacobgonzalez5252@hotmail.com>
2017-03-23 00:50:33 +1030

    command line flags and comments

--- wdn.go
+++ wdn.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,16 +12,20 @@ import (
 
 const title = "Watch Do Notify"
 
+var logfile = os.DevNull
+var display bool
+
+// notify shows an macOS notification
 func notify(subtitle string, text string) {
 	cmd := fmt.Sprintf("display notification \"%s\" with title \"%s\" subtitle \"%s\"", text, title, subtitle)
 
 	err := exec.Command("osascript", "-e", cmd).Run()
 	if err != nil {
 		log.Println("Failed to show notification.", err)
-		log.Println(cmd)
 	}
 }
 
+// do runs the cmd given by the user
 func do(script string) string {
 	out, err := exec.Command("bash", "-c", script).Output()
 	if err != nil {
@@ -29,6 +34,7 @@ func do(script string) string {
 	return string(out)
 }
 
+// getMod returns last modified time of file
 func getMod(file string) time.Time {
 	info, err := os.Stat(file)
 	if err != nil {
@@ -38,6 +44,7 @@ func getMod(file string) time.Time {
 	return info.ModTime()
 }
 
+// watch starts watching the files given in a new thread
 func watch(files []string, update chan bool) {
 	ticker := time.NewTicker(time.Second)
 
@@ -47,14 +54,22 @@ func watch(files []string, update chan bool) {
 	}
 
 	go func() {
+		// every second
 		for range ticker.C {
+			updated := false
 			for i, f := range files {
 				mod := getMod(f)
 				if mod != lastMod[i] {
 					lastMod[i] = mod
-					log.Printf("%v modified. running script\n", f)
-					update <- true
-					break
+					log.Println(f, "modified.")
+
+					// we only want to run the script once each round.
+					// to stop a "save-all" from running too many times
+					if !updated {
+						log.Println("running script\n", f)
+						updated = true
+						update <- true
+					}
 				}
 			}
 		}
@@ -62,22 +77,44 @@ func watch(files []string, update chan bool) {
 }
 
 func main() {
+	// command line flags
+	name := flag.String("name", "Saved", "string: name of command")
+	script := flag.String("cmd", "echo hello", "string: shell script to run")
+	logFilename := flag.String("log", "/dev/null", "string: logging output file")
+	display := flag.Bool("notify", false, "bool: should push macOS notification?")
 
-	if len(os.Args) < 4 {
-		fmt.Println("usage: wdn \"name\" \"cmd args\" files...")
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Println("need at least one file to watch")
 		return
 	}
-	name := os.Args[1]
-	script := os.Args[2]
-	files := os.Args[3:len(os.Args)]
 
-	fmt.Printf("running on files {%s}\n", strings.Join(files, ", "))
+	files := flag.Args()
 
+	// set logging file
+	if *logFilename == "/dev/null" {
+		logfile = os.DevNull
+	}
+
+	logfile, err := os.Create(*logFilename)
+	log.SetOutput(logfile)
+	if err != nil {
+		logfile = os.Stderr
+		log.SetOutput(logfile)
+		log.Println("failed to open log file.", err)
+	}
+
+	log.Printf("running on files {%s}\n", strings.Join(files, ", "))
+
+	// do main loop waiting on concurrent watch channel
 	update := make(chan bool)
 	watch(files, update)
 	for {
-		<-update
-		output := do(script)
-		notify(name, output)
+		<-update // wait for an update
+		output := do(*script)
+		if *display {
+			notify(*name, output)
+		}
 	}
 }
